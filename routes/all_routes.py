9b71bec2ab3c71b994c0944f91e87b9f4a34d6cc
Artur Gudiev <arturgudiev93@gmail.com>
2025-10-01 16:38:23 +0300

    Prepare t2 -- answer

--- routes/all_routes.py
+++ routes/all_routes.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 from .t1.a import router1
+from .t2.a import router as t2
 
 combined_router = APIRouter()
 
 combined_router.include_router(router1)
+combined_router.include_router(t2)
 
 
 
---
+++ routes/t2/a.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+from typing import Optional
+
+router = APIRouter()
+
+@router.get("/t2/{item_id}")
+async def path_operation_function(item_id: int = 'Default Value'):
+    return {"item_id": f'Here what I get item_id {item_id}'}
