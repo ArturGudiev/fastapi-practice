2f393af154644e3659d438c721458002fcb170da
Artur Gudiev <arturgudiev93@gmail.com>
2025-10-02 04:53:03 +0300

    Fixed ticket 6

--- routes/t6_parameters_and_string_validations/a.py
+++ routes/t6_parameters_and_string_validations/a.py
@@ -6,3 +6,9 @@ router = APIRouter()
 @router.get("/t6/min_max_string")
 def t6_handler(q: Annotated[str | None, Query(min_length=3, max_length=10)]):
     return {"your validated q": q}
+
+
+
+@router.get("/t6/regex")
+def t6_handler_regex(q: Annotated[str, Query(regex=r"^ab[a-z]+z$")]):
+    return {"your regex q": q}
--- routes/t6_parameters_and_string_validations/q.py
+++ routes/t6_parameters_and_string_validations/q.py
@@ -3,4 +3,10 @@ Write a method /t6/min_max_string that returns a dictionary
 { "your validated q": q }
 
 q is a query parameter with the following validation: min_length=3, max_length=10
+
+2) Write a method /t6/regex that returns a dictionary
+{ "your regex q": q }
+
+q is a query parameter with the following validation: regex=r"^ab[a-z]+z$"
+
 """
